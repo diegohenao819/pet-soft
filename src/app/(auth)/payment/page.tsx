2529209b1d11f91aa6175bc48f04818d6c92e9c2
diegohenao819 <diegohenao819@gmail.com>
2024-09-05 18:54:12 -0500

    wwewr

--- src/app/(auth)/payment/page.tsx
+++ src/app/(auth)/payment/page.tsx
@@ -1,18 +1,33 @@
 "use client";
 import { createCheckoutSession } from "@/actions/actions";
 import { Button } from "@/components/ui/button";
+import { useTransition } from "react";
 
-const page = () => {
+const page = ({ searchParams }) => {
+  const [isPending, startTransition] = useTransition();
   return (
     <main className="flex justify-center flex-col text-center gap-2">
       <h1>PetSoft requires a payment </h1>
-      <Button
-        onClick={async () => {
-          await createCheckoutSession();
-        }}
-      >
-        Buy lifetime access for $299
-      </Button>
+      {!searchParams.success && (
+        <Button
+          disabled={isPending}
+          onClick={async () => {
+            startTransition(async () => {
+              await createCheckoutSession();
+            });
+          }}
+        >
+          Buy lifetime access for $299
+        </Button>
+      )}
+
+      {searchParams.success && (
+        <h2 className="text-sm text-green-700">Payment successful!</h2>
+      )}
+
+      {searchParams.cancelled && (
+        <h2 className="text-sm text-red-700">Payment cancelled</h2>
+      )}
     </main>
   );
 };
