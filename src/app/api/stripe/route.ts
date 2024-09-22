df13ede9f149f189f75f2e30198b19ecb02df1cc
diegohenao819 <diegohenao819@gmail.com>
2024-09-22 17:26:58 -0500

    Refactor: Update POST function in stripe route.ts

--- src/app/api/stripe/route.ts
+++ src/app/api/stripe/route.ts
@@ -1,31 +1,17 @@
 import prisma from "@/lib/db";
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
 
 export async function POST(request: Request) {
-  const buf = await request.arrayBuffer();
-  const rawBody = Buffer.from(buf);
-  
-  const signature = request.headers.get("Stripe-Signature");
-  console.log("body", rawBody);
+  const body = await request.text();
+
+  console.log("body", body);
 
   // verify webhook came from Stripe
-  let event;
-  try {
-    event = stripe.webhooks.constructEvent(
-      rawBody,
-      signature,
-      process.env.STRIPE_WEBHOOK_SECRET
-    );
-  } catch (error) {
-    console.error("Webhook verification failed.", error);
-    return Response.json(null, { status: 400 });
-  }
+  
 
   // fullfill order
 
-  switch (event.type) {
-    case "checkout.session.completed":
-      console.log("Ya pagó maldita sea");
+  const event = JSON.parse(body);
       await prisma.user.update({
         where: {
           email: event.data.object.customer_email,
@@ -34,9 +20,8 @@ export async function POST(request: Request) {
           hasAccess: true,
         },
       });
-      break;
-    default:
-      console.log(`Unhandled event type ${event.type}`);
+   
+    
   }
 
   return new Response(null, {
